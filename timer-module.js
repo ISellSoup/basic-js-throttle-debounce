86a044823bdd4fd55423dc26d7598c43153dd081
ISellSoup <williamschaller30@gmail.com>
2026-02-04 20:09:03 -0600

    Create timer-module.js

---
+++ timer-module.js
@@ -0,0 +1,41 @@
+class Timer {
+    #throttleCount = 0;
+    #debounceCount = 0;
+    #debounceGen = 0;
+
+    constructor({ throttle = 0, throttleLimit = 1, debounce = 0 } = {}) {
+        this.throttle = throttle;
+        this.throttleLimit = throttleLimit;
+        this.debounce = debounce;
+    }
+
+    call(callback) {
+        if (!this.throttle || this.#throttleCount < this.throttleLimit) {
+            if (this.throttle) {
+                if (this.#throttleCount === 0) {
+                    setTimeout(() => { this.#throttleCount = 0; }, this.throttle);
+                }
+                this.#throttleCount++;
+            }
+
+            if (this.debounce) {
+                this.#debounceCount++;
+                const targetCount = this.#debounceCount;
+                const targetGen = this.#debounceGen;
+
+                setTimeout(()=>{
+                    if (this.#debounceCount === targetCount && this.#debounceGen === targetGen) {
+                        this.#debounceCount = 0;
+                        this.#debounceGen++;
+                        callback();
+                    }
+                },this.debounce);
+                return true;
+            }
+            callback();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
