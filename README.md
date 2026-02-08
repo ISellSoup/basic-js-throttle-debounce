973710041cf7be6f06d6629c7a20da0c50c26bda
ISellSoup <williamschaller30@gmail.com>
2026-02-08 16:40:40 -0600

    Refactor Timer to CallTimer with scheduling
    Rename and refactor the timer module into a named CallTimer export and introduce a scheduling API. The class now uses private fields for throttle tracking. Added a queue, getters/setters, and new methods: limitReached, force, registerDebounce, schedule, and forceScheduled. The debounce/throttle logic was reworked to drive queued calls when capacity frees up. demo.html was updated to import CallTimer, provide separate basic/schedule demos, new controls/buttons, and minor style tweaks. README.md was updated to document the new CallTimer properties and methods.

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # Basic js throttle/debounce module
-This module exports a `Timer` class. It's constructor includes in a table of options.
+This module exports a `CallTimer` class. The first argument should be a table of options.
 ```
 {
   throttle = 0,
-  throttleLimit = 1,
-  debounce = 0
+  debounce = 0,
+  throttleLimit = 1
 }
 ```
 ## Properties
 * `throttle` Throttle time in ms.
-* `throttleLimit` The # of calls allowed before future ones are throttled.
 * `debounce` Debounce time in ms.
-* `throttleCount` The property that keeps track of how many times a function was called.
+* `throttleLimit` The # of calls allowed before future ones are throttled.
+* `throttleCount` The property that keeps track of how many times `force()` was called, including internal calls.
+* `queue` An array of calls to make in the future. Each item should be an object with properties `beforeDebounce` and `debounce`.
 > [!NOTE]
-> The `throttle` property does not take effect immediatley. If a previous throttle timer was already started, changing this property will only take effect once it finishes.
+> The `throttle` property does not take effect immediately. If a previous throttle timer was already started, changing this property will only take effect once it finishes. 
 ## Methods
-This class only has one method, the `do(callback, beforeDebounce)` method. The callback parameter is called with throttle on debounce, and the beforeDebounce parameter is only called with throttle. Neither parameter is required. This method returns `false` if the call was throttled, and `true` if the call wasn't throttled.
+* `limitReached(): Boolean` Returns whether the throttle limit has been reached.
+* `do(beforeDebounce: Function = ()=>{}, debounce: Function = ()=>{}): Boolean` Returns `false` if the throttle limit has been reached. Otherwise returns `true` and calls `force(beforeDebounce, debounce)`.
+* `force(beforeDebounce: Function = ()=>{}, debounce: Function = ()=>{})` Starts throttle timer if `throttleCount` is a falsy value, increments `throttleCount`, calls `beforeDebounce()`, and calls `registerDebounce(debounce)` if `debounce` is a truthy value.
+* `registerDebounce(callback: Function = ()=>{})` Increments `#debounceCount` private field, assigns the updated value to a constant, and starts a debounce timer. Calls `callback()` when the timer finishes if the current value of `#debounceCount` is equal to the value of the constant.
+* `schedule(priority: Boolean, beforeDebounce: Function = ()=>{}, debounce: Function = ()=>{})` If `limitReached()` is false, `do(beforeDebounce, debounce)` is called immediately. Else, an object containing the passed functions is created and added to the start of `queue` (when `priority` is `true`) or end of `queue` (when `priority` is `false`).
+* `forceScheduled(count: Number = Infinity)` Slices out a portion of `queue` and iterates through it, calling `force(item.beforeDebounce, item.debounce)` for each item. Then, the other items of `queue` are sliced out into a new array, which is assigned to `queue`.
 
 The demo page is hosted [here](https://basic-js-throttle-debounce.pages.dev/demo).
--- timer-module.js
+++ timer-module.js
@@ -1,41 +1,85 @@
 // Module created by ISellSoup on GitHub
-export default class Timer {
+export class CallTimer {
     #debounceCount = 0;
+    #throttleLimit = 0;
+    #throttleCount = 0;
 
-    constructor({ throttle = 0, throttleLimit = 1, debounce = 0 } = {}) {
+    constructor({ throttle = 0, debounce = 0, throttleLimit = 1 } = {}) {
         this.throttle = throttle;
-        this.throttleLimit = throttleLimit;
         this.debounce = debounce;
-        this.throttleCount = 0;
+        this.#throttleLimit = throttleLimit;
+        this.#throttleCount = 0;
+        this.queue = [];
     }
 
-    do(callback = ()=>{},beforeDebounce = ()=>{}) {
-        if (!this.throttle || this.throttleCount < this.throttleLimit) {
-            if (this.throttle) {
-                if (this.throttleCount === 0) {
-                    setTimeout(() => { this.throttleCount = 0; }, this.throttle);
-                }
-                this.throttleCount++;
+    set throttleCount(value) {
+        this.#throttleCount = Math.max(value,0) || 0;
+        this.forceScheduled(this.#throttleLimit - this.#throttleCount);
+    }
+
+    set throttleLimit(value) {
+        this.#throttleLimit = Math.max(value,0) || 1;
+        this.forceScheduled(this.#throttleLimit - this.#throttleCount);
+    }
+
+    get throttleCount() {
+        return this.#throttleCount;
+    }
+
+    get throttleLimit() {
+        return this.#throttleLimit;
+    }
+
+    limitReached() {
+        return this.throttleCount >= this.throttleLimit && this.throttle;
+    }
+
+    do(beforeDebounce = ()=>{}, debounce = ()=>{}) {
+        if (!this.limitReached()) this.force(beforeDebounce, debounce);
+        else return false;
+        return true;
+    }
+
+    force(beforeDebounce = ()=>{}, debounce = ()=>{}) {
+        if (this.throttle) {
+            if (!this.throttleCount) {
+                setTimeout(() => { this.throttleCount = 0; }, this.throttle);
             }
-
-            if (this.debounce) {
-                if (this.#debounceCount >= Number.MAX_SAFE_INTEGER) this.#debounceCount = Number.MIN_SAFE_INTEGER;
-                else this.#debounceCount++;
-
-                const targetCount = this.#debounceCount;
-
-                setTimeout(()=>{
-                    if (this.#debounceCount === targetCount) {
-                        this.#debounceCount = 0;
-                        callback();
-                    }
-                },this.debounce);
-            } else callback();
-
-            if (beforeDebounce) beforeDebounce();
-            return true;
-        } else {
-            return false;
+            this.throttleCount++;
         }
+
+        if (beforeDebounce) beforeDebounce();
+
+        if (this.debounce) this.registerDebounce(debounce);
+        else debounce();
+    }
+
+    registerDebounce(callback = ()=>{}) {
+        if (this.#debounceCount >= Number.MAX_SAFE_INTEGER) this.#debounceCount = Number.MIN_SAFE_INTEGER;
+        else this.#debounceCount++;
+
+        const targetCount = this.#debounceCount;
+
+        setTimeout(()=>{
+            if (this.#debounceCount === targetCount) {
+                callback();
+            }
+        },this.debounce);
+    }
+
+    schedule(priority, beforeDebounce = ()=>{}, debounce = ()=>{}) {
+        if (this.limitReached()) {
+            const object = {"beforeDebounce": beforeDebounce, "debounce": debounce};
+
+            if (priority) this.queue.unshift(object);
+            else this.queue.push(object);
+        
+        } else this.do(beforeDebounce, debounce);
+    }
+
+    forceScheduled(count = Infinity) {
+        count = Math.min(Math.max(0, count), this.queue.length);
+        this.queue.slice(0, count).forEach((value) => { this.force(value.beforeDebounce, value.debounce) });
+        this.queue = this.queue.slice(count);
     }
 }
